b60df78293f1a1213ae0db1887f9568549865725
sienna0715 <5609373@naver.com>
2023-08-24 16:46:14 +0900

    ✏️style/fontface 추가 및 color, font 변수화 작업

---
+++ src/styles/fontStyles.ts
@@ -0,0 +1,149 @@
+import { css } from 'styled-components';
+
+export const FONT_STYLE_V1 = {
+  text: {
+    text_4_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 0.4rem;
+    `,
+    text_6_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 0.6rem;
+    `,
+    text_8_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 0.8rem;
+    `,
+    text_9_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 0.9rem;
+    `,
+    text_10_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1rem;
+    `,
+    text_12_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.2rem;
+    `,
+    text_13_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.3rem;
+    `,
+    text_14_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.4rem;
+    `,
+    text_15_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.5rem;
+    `,
+    text_16_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.6rem;
+    `,
+    text_17_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.7rem;
+    `,
+    text_18_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.8rem;
+    `,
+    text_19_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 1.9rem;
+    `,
+    text_20_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 2rem;
+    `,
+    text_21_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 2.1rem;
+    `,
+    text_22_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 2.2rem;
+    `,
+    text_24_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 2.4rem;
+    `,
+    text_26_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 2.6rem;
+    `,
+    text_28_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 2.8rem;
+    `,
+    text_30_light: css`
+      font-family: 'Pretendard-Light';
+      font-size: 3rem;
+    `,
+    text_8_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 0.8rem;
+    `,
+    text_9_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 0.9rem;
+    `,
+    text_10_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 1rem;
+    `,
+    text_12_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 1.2rem;
+    `,
+    text_14_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 1.4rem;
+    `,
+    text_16_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 1.6rem;
+    `,
+    text_18_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 1.8rem;
+    `,
+    text_20_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 2rem;
+    `,
+    text_25_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 2.5rem;
+    `,
+    text_30_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 3rem;
+    `,
+    text_35_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 3.5rem;
+    `,
+    text_40_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 4rem;
+    `,
+    text_45_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 4.5rem;
+    `,
+    text_50_medium: css`
+      font-family: 'Pretendard-Medium';
+      font-size: 5rem;
+    `,
+    text_100_bold: css`
+      font-family: 'Pretendard-ExtraBold';
+      font-size: 10rem;
+      line-height: 8rem;
+    `,
+  },
+};
+
+export const FONT_STYLE_V2 = {};
