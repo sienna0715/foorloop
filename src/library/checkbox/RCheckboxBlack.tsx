c19b2d6753c1e9b59e306af92d4a7868539fe9cf
sienna0715 <5609373@naver.com>
2023-08-28 22:17:51 +0900

    ✨feat/checkbox 컴포넌트 제작

---
+++ src/library/checkbox/RCheckboxBlack.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+// components
+import { PALETTE_COMPONENT } from '../../styles/colors';
+import Check from '../../assets/checkbox-beige.png';
+
+const CheckboxWrap = styled.div`
+  width: 25px;
+  height: 25px;
+`;
+const Checkbox = styled.div`
+  width: 25px;
+  height: 25px;
+  border: 1px solid ${PALETTE_COMPONENT.primary_black};
+  border-radius: 15px;
+`;
+const CheckboxFill = styled.div`
+  width: 25px;
+  height: 25px;
+  background-color: ${PALETTE_COMPONENT.primary_black};
+  border: 1px solid ${PALETTE_COMPONENT.primary_black};
+  border-radius: 15px;
+`;
+const CheckIcon = styled.img``;
+
+export default function RCheckboxBlack() {
+  const [check, isCheck] = useState(false);
+
+  return (
+    <CheckboxWrap onClick={() => isCheck(!check)}>
+      {check ? (
+        <CheckboxFill>
+          <CheckIcon src={Check} />
+        </CheckboxFill>
+      ) : (
+        <Checkbox />
+      )}
+    </CheckboxWrap>
+  );
+}
