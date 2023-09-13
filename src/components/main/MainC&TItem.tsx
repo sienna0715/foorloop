9261cc56725e4dc8051831ec93cc361411dde312
lapmu <antjdgkdl@gmail.com>
2023-09-13 16:30:38 +0900

    💄design/C&T 반응형 작업 완성

--- src/components/main/MainC&TItem.tsx
+++ src/components/main/MainC&TItem.tsx
@@ -1,11 +1,17 @@
 import { styled } from 'styled-components';
 import MainColorTypographyColor from './MainC&TColor';
 import MainColorTypographyTypography from './MainC&TTypography';
+import { BREAKPOINTDESKTOP } from '../../breakpoint';
 
 const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  @media screen and (max-width: ${BREAKPOINTDESKTOP}px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 function MainColorTypographyItem() {
