ded3f414189a7f9ee79fa7704882b448b2e6f9b4
lapmu <antjdgkdl@gmail.com>
2023-09-14 15:26:51 +0900

    🐛fix/dropdown 너비기준으로 유동적으로 변하게 수정

--- src/components/main/MainComponentItems.tsx
+++ src/components/main/MainComponentItems.tsx
@@ -21,7 +21,15 @@ function MainComponentItems() {
     { title: 'checkbox', component: <CheckboxRedLabel /> },
     { title: 'button', component: <PlusButton /> },
     { title: 'input', component: <InputDefault /> },
-    { title: 'dropdown', component: <DefaultDropdown /> },
+    {
+      title: 'dropdown',
+      component: (
+        <DefaultDropdown
+          width={200}
+          list={['옵션 1', '옵션 2', '옵션 3', '옵션 4', '옵션 5']}
+        />
+      ),
+    },
   ];
   return (
     <Container>
