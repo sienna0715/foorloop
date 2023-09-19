8b38a04270654d7e9c8c312cea49cb45e3137a48
sienna0715 <5609373@naver.com>
2023-09-19 16:20:32 +0900

    🐛fix/Search 컴포넌트 props 설정

--- src/library/search/Search.tsx
+++ src/library/search/Search.tsx
@@ -4,15 +4,18 @@ import styled from 'styled-components';
 import { PALETTE_COMPONENT } from '../../styles/colors';
 import { FONT_STYLE_V1 } from '../../styles/fontStyles';
 import DeleteImg from '../../assets/input-delete.svg';
-import SearchImg from '../../assets/search.svg';
+import { ReactComponent as SearchIcon } from '../../assets/search.svg';
 
-const SearchWrap = styled.div`
+const SearchWrap = styled.div<{ color: string }>`
   .focus {
-    border: 1px solid ${PALETTE_COMPONENT.primary_black};
+    border: 1px solid ${(props) => (props.color ? props.color : 'black')};
+    box-shadow: 0px 0px 9px 0px
+      ${(props) => (props.color ? props.color : 'black')};
   }
+  cursor: pointer;
 `;
-const SearchContainer = styled.div`
-  width: 400px;
+const SearchContainer = styled.div<{ width: number }, { color: string }>`
+  width: ${(props) => (props.width ? props.width : 400)}px;
   height: 60px;
   padding: 0.8rem;
   border: 1px solid ${PALETTE_COMPONENT.gray03};
@@ -20,14 +23,18 @@ const SearchContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  svg {
+    fill: ${(props) => (props.color ? props.color : 'black')};
+  }
 `;
-const SearchIcon = styled.img``;
-const Input = styled.input`
+const Input = styled.input<{ color: string }>`
   ${FONT_STYLE_V1.text.text_10_medium};
   width: 100%;
   margin: 0 10px;
   outline: none;
   border: none;
+  background-color: transparent;
+  color: ${(props) => (props.color ? props.color : 'black')};
   &::placeholder {
     color: ${PALETTE_COMPONENT.gray03};
   }
@@ -38,7 +45,12 @@ const DeleteBtn = styled.img`
   cursor: pointer;
 `;
 
-export default function Search() {
+interface ISearchProps {
+  width: number;
+  color: string;
+}
+
+export default function Search({ width = 400, color = 'black' }: ISearchProps) {
   const [value, setValue] = useState('');
   const [isFocus, setIsFocus] = useState(false);
   const count = value.length;
@@ -48,9 +60,13 @@ export default function Search() {
   };
 
   return (
-    <SearchWrap>
-      <SearchContainer className={isFocus ? 'focus' : ''}>
-        <SearchIcon src={SearchImg} />
+    <SearchWrap color={color}>
+      <SearchContainer
+        className={isFocus ? 'focus' : ''}
+        width={width}
+        color={color}
+      >
+        <SearchIcon />
         <Input
           type="text"
           placeholder="search..."
@@ -58,6 +74,7 @@ export default function Search() {
           onFocus={() => setIsFocus(true)}
           onBlur={() => setIsFocus(false)}
           onChange={handleChange}
+          color={color}
         />
         {count !== 0 && (
           <DeleteBtn src={DeleteImg} onClick={() => setValue('')} />
