8024b87af664659f607bbad3e95364214292e1bb
sienna0715 <5609373@naver.com>
2023-09-19 16:53:08 +0900

    💄design/크기 변경 및 정렬

--- src/components/component/ComponentsBottom.tsx
+++ src/components/component/ComponentsBottom.tsx
@@ -8,11 +8,19 @@ import datas from '../../data/componentsData';
 const ComponentsBottomWrap = styled.div`
   width: 100%;
   padding-top: 10rem;
+  display: flex;
+  justify-content: center;
+`;
+const ComponentsBottomContainer = styled.div`
+  width: 132rem;
+  display: flex;
+  flex-direction: column;
 `;
 const ComponentsBottomRow = styled.div`
-  width: 135rem;
+  width: 100%;
   display: flex;
   flex-wrap: wrap;
+  justify-content: flex-start;
   padding-bottom: 7rem;
 `;
 const Title = styled.div`
@@ -71,49 +79,51 @@ function ComponentsBottom() {
   // };
   return (
     <ComponentsBottomWrap>
-      <Title>Input</Title>
-      <ComponentsBottomRow>
-        {datas.inputs.map((item) => (
-          <ComponentBigBox key={item.id}>{item.component}</ComponentBigBox>
-        ))}
-      </ComponentsBottomRow>
-      <Title>Dropdown</Title>
-      <ComponentsBottomRow>
-        {datas.dropdowns.map((item) => (
-          <ComponentBigBox key={item.id}>{item.component}</ComponentBigBox>
-        ))}
-        {datas.searchs.map((item) => (
-          <ComponentBigBox key={item.id}>{item.component}</ComponentBigBox>
-        ))}
-      </ComponentsBottomRow>
-      <Title>Toggle</Title>
-      <ComponentsBottomRow>
-        {datas.toggles.map((item) => (
-          <ComponentBox key={item.id}>{item.component}</ComponentBox>
-        ))}
-      </ComponentsBottomRow>
-      <Title>Checkbox</Title>
-      <ComponentsBottomRow>
-        {datas.checkboxs.map((item) => (
-          <ComponentBox key={item.id}>{item.component}</ComponentBox>
-        ))}
-        {datas.darkCheckboxs.map((item) => (
-          <ComponentFilledBox key={item.id}>
-            {item.component}
-          </ComponentFilledBox>
-        ))}
-      </ComponentsBottomRow>
-      <Title>Button</Title>
-      <ComponentsBottomRow>
-        {datas.buttons.map((item) => (
-          <ComponentBox key={item.id}>{item.component}</ComponentBox>
-        ))}
-        {datas.darkButtons.map((item) => (
-          <ComponentFilledBox key={item.id}>
-            {item.component}
-          </ComponentFilledBox>
-        ))}
-      </ComponentsBottomRow>
+      <ComponentsBottomContainer>
+        <Title>Input</Title>
+        <ComponentsBottomRow>
+          {datas.inputs.map((item) => (
+            <ComponentBigBox key={item.id}>{item.component}</ComponentBigBox>
+          ))}
+        </ComponentsBottomRow>
+        <Title>Dropdown</Title>
+        <ComponentsBottomRow>
+          {datas.dropdowns.map((item) => (
+            <ComponentBigBox key={item.id}>{item.component}</ComponentBigBox>
+          ))}
+          {datas.searchs.map((item) => (
+            <ComponentBigBox key={item.id}>{item.component}</ComponentBigBox>
+          ))}
+        </ComponentsBottomRow>
+        <Title>Toggle</Title>
+        <ComponentsBottomRow>
+          {datas.toggles.map((item) => (
+            <ComponentBox key={item.id}>{item.component}</ComponentBox>
+          ))}
+        </ComponentsBottomRow>
+        <Title>Checkbox</Title>
+        <ComponentsBottomRow>
+          {datas.checkboxs.map((item) => (
+            <ComponentBox key={item.id}>{item.component}</ComponentBox>
+          ))}
+          {datas.darkCheckboxs.map((item) => (
+            <ComponentFilledBox key={item.id}>
+              {item.component}
+            </ComponentFilledBox>
+          ))}
+        </ComponentsBottomRow>
+        <Title>Button</Title>
+        <ComponentsBottomRow>
+          {datas.buttons.map((item) => (
+            <ComponentBox key={item.id}>{item.component}</ComponentBox>
+          ))}
+          {datas.darkButtons.map((item) => (
+            <ComponentFilledBox key={item.id}>
+              {item.component}
+            </ComponentFilledBox>
+          ))}
+        </ComponentsBottomRow>
+      </ComponentsBottomContainer>
     </ComponentsBottomWrap>
   );
 }
