ce6f336530ec780f96fad4ee951e02d8431e58cf
sienna0715 <5609373@naver.com>
2023-09-27 00:55:19 +0900

    💄design/토글 버튼 및 title 배치

--- src/components/component/ComponentsTop.tsx
+++ src/components/component/ComponentsTop.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 // components
 import { PALLETTE_MAIN } from '../../styles/colors';
-// import componentsData from '../../data/componentsData';
+import { FONT_STYLE_V1 } from '../../styles/fontStyles';
 import codeDatas from '../../data/componentsCodeDatas';
 import CodeBlock from '../CodeBlock';
 
@@ -13,15 +14,89 @@ const ComponentsTopWrap = styled.div`
   padding-bottom: 26rem;
   border-bottom: 1px solid ${PALLETTE_MAIN.sub_main};
 `;
-const Preview = styled.div`
-  width: 60rem;
-  height: 60rem;
-  background-color: ${PALLETTE_MAIN.sub_main};
-  border-radius: 3rem;
+const PreviewBox = styled.div`
   margin-right: 13rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+const CodeBlockBox = styled.div``;
+
+const Preview = styled.div<{ $fill: string }>`
+  width: 60rem;
+  height: 58rem;
+  background-color: ${(props) =>
+    props.$fill === 'true' ? PALLETTE_MAIN.main : PALLETTE_MAIN.sub_main};
+  border: 2px solid ${PALLETTE_MAIN.sub_main};
+  border-radius: 3rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
+`;
+const BackgroundToggle = styled.div<{ $fill: string }>`
+  ${FONT_STYLE_V1.text.text_24_medium}
+  width: 12rem;
+  height: 5rem;
+  margin-bottom: 2rem;
+  padding: 0 0.75rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: 1px solid ${PALLETTE_MAIN.sub_main};
+  border-radius: 2.5rem;
+  background-color: ${(props) =>
+    props.$fill === 'true' ? PALLETTE_MAIN.sub_main : PALLETTE_MAIN.main};
+  color: ${(props) =>
+    props.$fill === 'true' ? PALLETTE_MAIN.main : PALLETTE_MAIN.sub_main};
+  transition: background-color 0.5s ease-in;
+  cursor: pointer;
+`;
+const BackgroundToggleDot = styled.div<{ $fill: string }>`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  background-color: ${(props) =>
+    props.$fill === 'true' ? PALLETTE_MAIN.main : PALLETTE_MAIN.sub_main};
+  transform: ${(props) =>
+    props.$fill === 'true' ? 'translateX(40%)' : 'none'};
+  transition: background-color transform 0.5s ease-in;
+`;
+const Title = styled.div`
+  width: 100%;
+  margin-bottom: 4rem;
+  display: flex;
+  align-items: center;
+`;
+const TitleDot = styled.div`
+  width: 2rem;
+  height: 2rem;
+  margin-right: 2rem;
+  background-color: ${PALLETTE_MAIN.sub_main};
+  border-radius: 50%;
+`;
+const TitleText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${FONT_STYLE_V1.text.text_18_medium}
+  font-weight: 400;
+  color: ${PALLETTE_MAIN.sub_main};
+`;
+const ControlBar = styled.div``;
+const ControlTitle = styled.div`
+  width: 18rem;
+  height: 3.6rem;
+  margin-bottom: 5rem;
+  margin-right: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid ${PALLETTE_MAIN.sub_main};
+  border-radius: 1.8rem;
+  ${FONT_STYLE_V1.text.text_24_medium}
+  font-weight: 400;
+  color: ${PALLETTE_MAIN.sub_main};
 `;
 
 interface IComponentsTopProps {
@@ -29,12 +104,40 @@ interface IComponentsTopProps {
 }
 
 function ComponentsTop({ currentItem }: IComponentsTopProps) {
+  const [isFill, setIsFill] = useState('false');
   const datas = codeDatas[+currentItem];
+
+  const isFillClickHandler = () => {
+    if (isFill === 'true') {
+      setIsFill('false');
+    } else if (isFill === 'false') {
+      setIsFill('true');
+    }
+  };
 
   return (
     <ComponentsTopWrap>
-      <Preview>{datas?.component}</Preview>
-      <CodeBlock titles={['React', 'Styled-components']} codes={datas?.code} />
+      <PreviewBox>
+        <BackgroundToggle onClick={isFillClickHandler} $fill={isFill}>
+          {isFill === 'true' ? 'Fill' : null}
+          <BackgroundToggleDot $fill={isFill} />
+          {isFill === 'true' ? null : 'Empty'}
+        </BackgroundToggle>
+        <Preview $fill={isFill}>{datas?.component}</Preview>
+      </PreviewBox>
+      <CodeBlockBox>
+        <Title>
+          <TitleDot />
+          <TitleText>{`component > ${datas?.name} icon`}</TitleText>
+        </Title>
+        <ControlBar>
+          <ControlTitle>Size</ControlTitle>
+        </ControlBar>
+        <CodeBlock
+          titles={['React', 'Styled-components']}
+          codes={datas?.code}
+        />
+      </CodeBlockBox>
     </ComponentsTopWrap>
   );
 }
