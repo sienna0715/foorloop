cbf081e1131aadc63421db5de608843412672926
sienna0715 <5609373@naver.com>
2023-09-26 17:22:23 +0900

    ⚡️improve/App 최적화 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // component
-import Main from './pages/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Components from './pages/Components';
-import Icons from './pages/Icons';
+
+const Main = React.lazy(() => import('./pages/Main'));
+const Components = React.lazy(() => import('./pages/Components'));
+const Icons = React.lazy(() => import('./pages/Icons'));
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/components" element={<Components />} />
-        <Route path="/icons" element={<Icons />} />
+        <Route
+          path="/"
+          element={
+            <Suspense>
+              <Main />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/components"
+          element={
+            <Suspense>
+              <Components />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/icons"
+          element={
+            <Suspense>
+              <Icons />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
