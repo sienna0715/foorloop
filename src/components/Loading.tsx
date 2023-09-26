9346aa53075b090f356bcc75d277db2a4288190e
sienna0715 <5609373@naver.com>
2023-09-26 16:46:49 +0900

    ✨feat/loading 파일 생성

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { FONT_STYLE_V1 } from '../styles/fontStyles';
+import { PALLETTE_MAIN } from '../styles/colors';
+
+const Text = styled.p`
+  ${FONT_STYLE_V1.text.text_16_medium}
+  color: ${PALLETTE_MAIN.main}
+`;
+
+function Loading() {
+  return <Text>Loading...</Text>;
+}
+
+export default Loading;
